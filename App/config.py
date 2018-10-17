0b01b622847d928bccbfd9fa5ced2fa47d903dc5
LDrago <aneeshjain203@gmail.com>
2018-10-17 18:05:41 +0530

    remove password

--- App/config.py
+++ App/config.py
@@ -1,6 +1,6 @@
 API_KEY = "http://www.omdbapi.com/?apikey=e7d8f97a&type=series&t="
 SENDER = "aneeshjain203@gmail.com"
-PASSWORD = "hypernova540"
+PASSWORD = ""
 
 DB_USERNAME = "root"
 DB_PASSWORD = "ajs123"
